bf6bb99f36418339233b00fa0399032af1405f03
Liz Tiller <elizabeth.tiller@gmail.com>
2016-12-13 16:36:15 -0500

    words working, need to fix snowman and points

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,7 @@ const ALPHABET = _.range(26).map(i => String.fromCharCode(i + 97))
 
 // WORDS is an array of 1024 different seven letter words
 const WORDS = require('raw!../wordList.txt').trim().split('\n')
+// \n = new line in a string
 
 class App extends Component {
 
@@ -16,11 +17,16 @@ class App extends Component {
     super()
     // TODO
     this.state = {
+      word: _.sample(WORDS),
+      guesses: []
     }
   }
 
   choose (letter) {
-    // TODO
+    this.setState({
+      guesses: [...this.state.guesses, letter]
+      // takes guesses array and adds letter to the existing guesses.  Jason's preferred method over concat.  USE FOR TIC TAC TOE TOO
+    })
     console.log('You clicked', letter)
   }
 
@@ -30,17 +36,22 @@ class App extends Component {
   }
 
   render () {
+    const letters = ALPHABET.map((letter, i) => {
+      return <LetterButton
+        value={letter}
+        onChoose={() => this.choose(letter)}
+        disabled={this.state.guesses.includes(letter)}
+        key={i} />
+        // fix disabled
+    })
+
     return <div className='app'>
       <main>
         <Snowman step={this.points} size={400} />
         {/* TODO */}
-        <Word value='SNOWMAN' guesses={['E', 'M', 'O']} />
+        <Word value={this.state.word} guesses={this.state.guesses} />
         <div className='keyboard'>
-          {/* TODO */}
-          <LetterButton
-            value='A'
-            onChoose={() => this.choose('A')}
-            disabled={false} />
+          {letters}
         </div>
       </main>
       <footer>It's like hangman, but, um... backwards or something.</footer>
